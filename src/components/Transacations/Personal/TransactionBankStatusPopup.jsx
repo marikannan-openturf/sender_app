9ec64e36ea0ca41dcb18f2ade704e37d5081f4bb
marikannan-openturf <marikannan.p@openturf.in>
2022-02-08 19:25:05 +0530

    local changes added

--- src/components/Transacations/Personal/TransactionBankStatusPopup.jsx
+++ src/components/Transacations/Personal/TransactionBankStatusPopup.jsx
@@ -2,7 +2,7 @@ import { Button, Dialog, Stack, Typography, IconButton, DialogTitle } from '@mui
 import { CloseRounded } from '@mui/icons-material'
 import { styled } from '@mui/system'
 
-export default function TransactionMobileStatusPopup(props) {
+export default function TransactionBankStatusPopup(props) {
   const CustomButtom = styled(Button)`
     &.Mui-disabled{
        opacity:0.5;
@@ -15,7 +15,7 @@ export default function TransactionMobileStatusPopup(props) {
     >
       <DialogTitle>
         <Stack spacing={12} justifyContent='end' direction='row'>
-          <Typography variant='h6' fontFamily='Poppins' fontWeight='600' my={2}> Transaction Mobile Status</Typography>
+          <Typography variant='h6' fontFamily='Poppins' fontWeight='600' my={2}> Transaction Bank Status</Typography>
           <IconButton sx={{ justifyContent: 'right', marginBlockEnd: `auto !important`, marginLeft: 'auto' }} p={0}><CloseRounded width={10} height={10} onClick={() => props.setFeaturedInfoClose()} /></IconButton>
         </Stack>
       </DialogTitle>
